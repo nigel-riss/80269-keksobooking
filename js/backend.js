60685d6dc9dfeefa3b4a180f3bf8100f158214bb
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-12-18 13:50:06 +0200

    Add backend.js

---
+++ js/backend.js
@@ -0,0 +1,65 @@
+'use strict';
+
+(function () {
+  var loadURL = 'https://js.dump.academy/keksobooking/data';
+  var saveURL = 'https://js.dump.academy/keksobooking';
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', loadURL);
+    xhr.send();
+  };
+
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('POST', saveURL);
+    xhr.send(data);
+  };
+
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
